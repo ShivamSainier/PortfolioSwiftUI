14cc6ab5832fb2fb4eb723ab8c3b709f9744fc66
sonusainier <rohitsaini@lambdatest.com>
2021-08-14 14:07:11 +0530

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Rohit Saini on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
